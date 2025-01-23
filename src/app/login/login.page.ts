e9c3bd31b230e909561822204f55e0915a85308d
DHZ1LL1 <dhzilli100@outlook.com>
2025-01-23 09:00:01 -0600

    Implementar formularios

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { defaultEquals } from '@angular/core/primitives/signals';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +20,25 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  login(){
+  login(form:NgForm){
+
+    //Obtener valores del formulario
+    console.log('Email: ', this.email);
+    console.log('Password: ', this.password);
+    console.log(form.value);
+
+    //Validar datos del formulario
+    console.log('Formulario valido: ', form.valid);
+
+    //Si form es invalido
+    //Console.log ('Todos los campos son requeridos');
+    //return
+
+    if (form.invalid) {
+      console.log('Todos los campos son requeridos');
+      return;
+    }
+
     if (this.email === "admin") {
       console.log("Login exitoso");
     } else {
